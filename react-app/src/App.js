e576f4e5a44fcbbf7a7b772e5330b798af69aef5
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-01-20 23:10:33 -0500

    Creacion componentes base

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,10 @@
 
 import React from "react";
 import { TodoCounter } from "./TodoCounter"; 
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButton } from "./CreateTodoButton.js";
 
 const todos = [
   {text: 'Cortar Cebolla', completed: false},
@@ -14,15 +18,13 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-      {/*<TodoSearch />*/}
-      <input placeholder='Cebolla'/>
-      {/*<TodoList>
+      <TodoSearch />
+      <TodoList>
         {todos.map(todo => (
           <TodoItem />
         ))}
-      /*</TodoList>*/}
-      {/*<CreateTodoButton />*/}
-      <button>+</button>
+      </TodoList>
+      <CreateTodoButton />
     </React.Fragment>
   );
 }
---
+++ react-app/src/CreateTodoButton.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function CreateTodoButton(){
+    return(
+        <button>+</button>
+    );
+}
+
+export { CreateTodoButton};
