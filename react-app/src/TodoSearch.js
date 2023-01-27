dcb8cabc9e28fc15533bfb67139a864145f2017b
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-01-26 21:20:30 -0500

    eventos de item y search

--- react-app/src/TodoSearch.js
+++ react-app/src/TodoSearch.js
@@ -2,8 +2,16 @@ import react from "react";
 import './TodoSearch.css';
 
 function TodoSearch(){
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    };
+    
     return(
-        <input className="TodoSearch" placeholder="Cebolla" />
+        <input 
+            className="TodoSearch" 
+            placeholder="Cebolla" 
+            onChange={onSearchValueChange}
+        />
     );
 }
 export {TodoSearch};
