96dccec7101f2bc5e4e2fef2422a2ad99d13c385
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-01-26 21:07:56 -0500

    manejo de eventos con JS en React

--- react-app/src/CreateTodoButton.js
+++ react-app/src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import react from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(){
+    const onClickButton = (msg) => {
+        alert (msg);
+    }
+    
     return(
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton"
+            onClick={onClickButton("TODO: Abrir modal")}
+        >
+            +
+        </button>
     );
 }
 
