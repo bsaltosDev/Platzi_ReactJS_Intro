be1da9f7a81198659b06d464d12df8271f7c0b32
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-02-04 22:30:26 -0500

    Refactor de tag  de Consumer a uso de variable con React.useContext

--- react-app/src/App/AppUI.js
+++ react-app/src/App/AppUI.js
@@ -8,29 +8,33 @@ import { TodoItem } from "../TodoItem/index.js";
 import { CreateTodoButton } from "../CreateTodoButton/index.js";
 
 function AppUI() {
+    const {
+        error, 
+        loading, 
+        searchedTodos, 
+        completeTodo, 
+        deleteTodo,
+    } = React.useContext(TodoContext);
+    
     return (
         <React.Fragment>
             <TodoCounter />
             <TodoSearch />
-            <TodoContext.Consumer>
-                {({error, loading, searchedTodos, completeTodo, deleteTodo,}) => (
-                    <TodoList>
-                    {error && <p>Error...</p>}
-                    {loading && <p>Cargando...</p>}
-                    {(!loading && !searchedTodos.length) && <p>Cree su primer ToDo...</p>}
-    
-                    {searchedTodos.map(todo => (
-                    <TodoItem 
-                        key={todo.text} 
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodo(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                    />
-                    ))}
-                </TodoList>
-                )}
-            </TodoContext.Consumer>
+            <TodoList>
+            {error && <p>Error...</p>}
+            {loading && <p>Cargando...</p>}
+            {(!loading && !searchedTodos.length) && <p>Cree su primer ToDo...</p>}
+
+            {searchedTodos.map(todo => (
+            <TodoItem 
+                key={todo.text} 
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+            />
+            ))}
+        </TodoList>
             <CreateTodoButton />
         </React.Fragment>
     );
