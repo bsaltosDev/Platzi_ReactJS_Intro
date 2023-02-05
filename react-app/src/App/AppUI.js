b57a33c81f34b4bc8efb65d8afdce4a5783410dd
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-02-04 22:52:42 -0500

    incorporar un portal con ReacDom.createPortal

--- react-app/src/App/AppUI.js
+++ react-app/src/App/AppUI.js
@@ -6,6 +6,7 @@ import { TodoSearch } from "../TodoSearch/index.js";
 import { TodoList } from "../TodoList/index.js";
 import { TodoItem } from "../TodoItem/index.js";
 import { CreateTodoButton } from "../CreateTodoButton/index.js";
+import { Modal } from "../Modal/index.js";
 
 function AppUI() {
     const {
@@ -21,20 +22,23 @@ function AppUI() {
             <TodoCounter />
             <TodoSearch />
             <TodoList>
-            {error && <p>Error...</p>}
-            {loading && <p>Cargando...</p>}
-            {(!loading && !searchedTodos.length) && <p>Cree su primer ToDo...</p>}
+                {error && <p>Error...</p>}
+                {loading && <p>Cargando...</p>}
+                {(!loading && !searchedTodos.length) && <p>Cree su primer ToDo...</p>}
 
-            {searchedTodos.map(todo => (
-            <TodoItem 
-                key={todo.text} 
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-            />
-            ))}
-        </TodoList>
+                {searchedTodos.map(todo => (
+                <TodoItem 
+                    key={todo.text} 
+                    text={todo.text}
+                    completed={todo.completed}
+                    onComplete={() => completeTodo(todo.text)}
+                    onDelete={() => deleteTodo(todo.text)}
+                />
+                ))}
+             </TodoList>
+             <Modal>
+                <p>Test modal</p>
+             </Modal>
             <CreateTodoButton />
         </React.Fragment>
     );
