5c2bdfb6a2b0874ac0b25230a25db0bb2288d428
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2023-02-11 19:13:26 -0500

    abrir y cerrar modal con function prevState

--- react-app/src/CreateTodoButton/index.js
+++ react-app/src/CreateTodoButton/index.js
@@ -3,7 +3,7 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton(props){
     const onClickButton = (msg) => {
-       props.setOpenModal(true);
+       props.setOpenModal(prevState => !prevState);//uso una function q devuelve la negacion del estado anterior
     }
     
     return(
